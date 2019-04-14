c5d3df4261c0fef9ccc21034df5e88ca6f8f3cec
Alipudina <alipudina333@gmail.com>
2019-04-14 20:09:58 +0200

    some chenges in foto size

--- src/components/ProfileText.js
+++ src/components/ProfileText.js
@@ -5,12 +5,12 @@ export default class ProfileText extends Component {
   render() {
     return(
       <div className="profile-text">
-      <p className="wellcome-text">Welcome to my profile page</p>
-      <div className="pesonal-details">
-        <p>First name: Ali Mohammad</p>
-        <p>Last name: Pudina</p>
-        <p>Birthday: 23.07.1982</p>
-      </div>
+        <div className="personal-details">
+          <p>First name: Ali Mohammad</p>
+          <p>Last name: Pudina</p>
+          <p>Birthday: 23.07.1982</p>
+        </div>
+        <p className="wellcome-text">Welcome to my profile page</p>
       </div>
     )
   }
